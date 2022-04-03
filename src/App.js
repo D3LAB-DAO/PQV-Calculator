02f0c1da1c2d40fe647b983c3f969518c0636329
Ingeun Kim <ingeun92@naver.com>
2022-04-03 16:42:46 +0900

    append new components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from "./components/footer";
 import SubHeader from "./components/subheader";
 import CardTitle from "./components/cardtitle";
 import PQV from "./components/pqv";
+import Sponsors from "./components/sponsors";
+import Creators from "./components/creators";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <SubHeader />
         <CardTitle />
         <PQV />
+        <Sponsors />
+        <Creators />
       </body>
       <footer>
         <Footer />
